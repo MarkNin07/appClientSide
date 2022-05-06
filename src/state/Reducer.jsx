b95dfcfcf4cb28ecc78f1ab18b02dc9fa115984f
Marcos Nino <tmnino@uninorte.edu.co>
2022-05-06 11:22:38 -0500

    Reducer
    Reducer component was created

---
+++ src/state/Reducer.jsx
@@ -0,0 +1,24 @@
+
+function reducer(state, action){
+    switch(action.type){
+        case 'add-category':
+            return state
+
+        case 'remove-category':
+            return state
+
+        case 'get-categories':
+            return state
+        
+        case 'add-chores':
+            return state
+
+        case 'remove-chores':
+            return state
+
+        case 'update-chores':
+            return state
+
+        
+    }
+}
